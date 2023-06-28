0112f5112157be81a8850adb846887ca5949029d
ksrivera <ksrivera7@gmail.com>
2023-06-28 21:08:33 +0800

    test

---
+++ src/app/(index)/portfolio/category/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'Portfolio | UCode Clinic',
+  description: 'UCode Clinic Portfolio Page',
+}
+
+const Category = () => {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
---
+++ src/app/(index)/portfolio/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'Portfolio | UCode Clinic',
+  description: 'UCode Clinic Portfolio Page',
+}
+
+const PortfolioPage = () => {
+  return (
+    <div>PortfolioPage</div>
+  )
+}
+
+export default PortfolioPage
