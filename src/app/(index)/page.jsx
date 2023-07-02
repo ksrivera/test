99b1f5eee126d64437220e43c41f6eb888684d4c
ksrivera <ksrivera7@gmail.com>
2023-07-03 02:17:18 +0800

    index

--- src/app/(index)/page.jsx
+++ src/app/(index)/page.jsx
@@ -1,14 +1,9 @@
-'use client'
+
 
 import BackToTop from '@/components/BackToTop/BackToTop'
-import BarChart from '@/components/BarChart/BarChart'
+
 import Accordion from '@/components/accordion/Accordion'
 import Hero from '@/components/hero/Hero'
-import { useEffect } from 'react'
-import useSWR from 'swr'
-import { Alert } from "flowbite-react";
-import { Dropdown } from "flowbite-react";
-import CardComponent from '@/components/CardComponent/CardComponent'
 
 //import "tw-elements/dist/css/tw-elements.min.css";
 
